168e4b9ec8ed8619697d4d52408c2d50dd2bfcd8
Krasav4ik <zkonst12@gmail.com>
2020-10-03 22:23:32 +0300

    pr3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,80 +1,124 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+abstract class Robot {
+    protected String model = "Robot";
+    protected int capacity = 100;
 
-class Football extends JFrame
-{
-    Integer Madrid_score = 0;
-    Integer Milan_score = 0;
-    String lscorer = "";
-    String win = "Draw";
-    JButton but1 = new JButton("Milan");
-    JButton but2 = new JButton("Madrid");
-    JLabel score = new JLabel("Milan " + Milan_score + " X " + Madrid_score + " Madrid");
-    JLabel lastScorer = new JLabel("Last scorer: " + lscorer);
-    JLabel winner = new JLabel("Winner:" + win);
-    public Football()
-    {
-        setLayout(null);
-        but1.setBounds(100,300,100,50);
-        but2.setBounds(300,300,100,50);
-        score.setBounds(150, 200, 300,40);
-        lastScorer.setBounds(250, 350, 150, 40);
-        winner.setBounds(250, 400, 150, 40);
-        add(but1);
-        add(score);
-        add(but2);
-        add(lastScorer);
-        add(winner);
-        setSize(500,500);
+    public Robot(){}
 
-        but1.addMouseListener(new MouseListener()
-        {
-            public void mouseExited(MouseEvent a){}
-            public void mouseClicked(MouseEvent a)
-            {
-                Milan_score++;
-                lscorer = "Milan";
-                lastScorer.setText("Last scorer: " + lscorer);
-                score.setText("Milan " + Milan_score + " X " + Madrid_score + " Madrid");
-                if(Milan_score > Madrid_score){
-                    win = "Milan";
-                    winner.setText("Winner: " + win);
-                }else if(Milan_score==Madrid_score){
-                    win = "Draw";
-                    winner.setText("Winner: " + win);}
-            }
-            public void mouseEntered(MouseEvent a) {}
-            public void mouseReleased(MouseEvent a) {}
-            public void mousePressed(MouseEvent a) {}
-        });
-
-        but2.addMouseListener(new MouseListener()
-        {
-            public void mouseExited(MouseEvent a){}
-            public void mouseClicked(MouseEvent a)
-            {
-                Madrid_score++;
-                lscorer = "Madrid";
-                lastScorer.setText("Last scorer: " + lscorer);
-                score.setText("Milan " + Milan_score + " X " + Madrid_score + " Madrid");
-                if(Madrid_score > Milan_score){
-                    win = "Madrid";
-                    winner.setText("Winner: " + win);
-                } else if(Milan_score==Madrid_score){
-                    win = "Draw";
-                    winner.setText("Winner: " + win);}
-            }
-            public void mouseEntered(MouseEvent a) {}
-            public void mouseReleased(MouseEvent a) {}
-            public void mousePressed(MouseEvent a) {}
-        });
+    public Robot(String model, int capacity) {
+        this.model = model;
+        this.capacity = capacity;
     }
 
-    public static void main(String[]args)
-    {
-        new Football().setVisible(true);
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public static void main(String args[]) {
+        Vacuumcleaner R2D2 = new Vacuumcleaner(50);
+        Killer Ux32 = new Killer();
+
+        System.out.println(Ux32.getCapacity() + " " +
+                Ux32.getModel() + " " +
+                Ux32.getBullets());
+        Ux32.working();
+
+        System.out.println(R2D2.getCapacity()+" "+
+        R2D2.getModel()+ " " + R2D2.getPower());
+        R2D2.working();
+        System.out.println();
+
+
+    }
+}
+
+class Vacuumcleaner extends Robot{
+    private int power;
+    private final String model = "vacuumcleaner";
+    public Vacuumcleaner(){}
+
+    public Vacuumcleaner(int power){
+        this.power = power;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getCapacity() {
+        return super.getCapacity();
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+
+    public void setModel(String model) {
+        super.setModel(model);
+    }
+
+    void working(){
+        System.out.println("Cleaning rooms");
+    }
+}
+
+class Killer extends Robot{
+    private int bullets = 10;
+    private final String model = "Killer";
+
+    public Killer(){}
+
+    public Killer(int bullets){
+        this.bullets = bullets;
+    }
+
+    void working(){
+        System.out.println("Killing people");
+    }
+
+    public void setModel(String model) {
+        super.setModel(model);
+    }
+
+    public int getCapacity() {
+        return super.getCapacity();
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setCapacity(int capacity) {
+        super.setCapacity(capacity);
+    }
+
+    public void setBullets(int bullets) {
+        this.bullets = bullets;
+    }
+
+    public int getBullets() {
+        return bullets;
     }
 }
