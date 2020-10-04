03a84c85ccc5edafb9790babee3b4720ff1967b6
Krasav4ik <zkonst12@gmail.com>
2020-10-04 10:49:24 +0300

    pr5.15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,17 @@
 package com.company;
 
-class Main {
-    public static String main(int n) {
+public class Main {
+    public static int main(int n) {
         // Базовый случай
         if (n < 10) {
-            return Integer.toString(n);
-        } // Шаг рекурсии / рекурсивное условие
+            return n;
+        }// Шаг рекурсии / рекурсивное условие
         else {
-            return main(n / 10) + " " + n % 10;
+            System.out.print(n % 10 + " ");
+            return main(n / 10);
         }
     }
     public static void main(String[] args) {
-        System.out.println(main(153)); // вызов рекурсивной функции
+        System.out.println(main(123)); // вызов рекурсивной функции
     }
 }
