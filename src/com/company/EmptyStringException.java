fb3dc1a7fa848e148e06385b05fd8c83c9be6f4d
Krasav4ik <zkonst12@gmail.com>
2020-12-20 01:04:29 +0300

    pr 10 done

--- src/com/company/EmptyStringException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class EmptyStringException extends Exception {
-    EmptyStringException(String errorMessage){
-        super(errorMessage);
-    }
-}
--- src/com/company/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class StudentNotFoundException extends Exception {
-
-    StudentNotFoundException(String errorMessage){
-        super(errorMessage);
-    }
-}
